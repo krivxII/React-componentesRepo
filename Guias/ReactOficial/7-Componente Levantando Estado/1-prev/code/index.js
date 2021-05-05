66a7a8a042cd395fba123d950503a843d312b56c
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-05-05 17:46:01 +0200

    ejercicio de levantamiento de estado

---
+++ Guias/ReactOficial/7-Componente Levantando Estado/1-prev/code/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculator from "./Calculator.jsx";
+
+ReactDOM.render( <Calculator temperature={200} ></Calculator>, document.getElementById("root"));
