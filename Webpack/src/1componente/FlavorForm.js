8d61c0f807549b6fc05e381fb27af366879977dd
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-05-08 00:26:02 +0200

    -

---
+++ Webpack/src/1componente/FlavorForm.js
@@ -0,0 +1,47 @@
+import React, { Component} from "react";
+import {hot} from "react-hot-loader";
+
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default hot(module)(FlavorForm)
+
+/**
+ * You can pass an array into the value attribute, allowing you to select multiple options in a select tag:
+
+      <select multiple={true} value={['B', 'C']}>
+ * 
+ */
